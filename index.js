48bceb588fc4763083b52ef9ead636fa410c736c
sanatVibhor <sanatvibhor2@gmail.com>
2024-04-22 00:16:27 +0530

    added OS module-Routes

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-const Person=require('./person') //common JS method
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-const person1=new Person('John', 30);
-person1.greeting();
+//making a serve robject
+const server=http.createServer((req,res)=>{
+if(req.url==='/'){
+    fs.readFile(
+        path.join(__dirname,'public','index.html'),
+        (err,content)=>{ //data is called content here
+            if(err) throw err;
+            res.writeHead(200,{'content-type': 'index.html'});
+            res.end(content); //we want to show the content of the file
+        }
+    )
+}
+if(req.url==='/about'){
+    fs.readFile(
+        path.join(__dirname,'public','about.html'),
+        (err,content)=>{ //data is called content here
+            if(err) throw err;
+            res.writeHead(200,{'content-type': 'about.html'});
+            res.end(content); //we want to show the content of the file
+        }
+    )
+}
+   
+});
+const PORT=process.env.PORT||3000;
+
+server.listen(PORT,()=>console.log(`sever running on ${PORT}`));
---
+++ refrence/url_demo.js
@@ -0,0 +1,5 @@
+const url=require('url');
+const myUrl=new URL('https://www.youtube.com/watch?v=fBNz5xF-Kx4');
+
+//serialosed URL 44:29
+
