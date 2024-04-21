7788bf6e0ce08421c3cbb5212c2d091e245ec9ea
sanatVibhor <sanatvibhor2@gmail.com>
2024-04-21 19:43:09 +0530

    OS-Module

---
+++ refrence/os_demo.js
@@ -0,0 +1,12 @@
+const os=require('os');
+
+//platform
+console.log(os.platform());
+//CPU Architecture
+console.log(os.arch());
+//CPU Core info
+console.log(os.cpus());
+//free memory
+console.log(os.freemem())
+//total memory
+console.log(os.totalmem())
